9c4185c2667299c24816cc773073d1dc190edc1b
muchen <1813683393@qq.com>
2019-10-24 20:59:15 +0800

    'init'

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@
 应用根组件
 */
 import React, {Component} from 'react'
-import {Button, message} from 'antd'
-import {HashRouter, BrowserRouter, Route, Switch} from 'react-router-dom'
-import Login from "./pages/login/login"
-import Admin from "./pages/admin/admin"
+import {Router, Route, Switch} from 'react-router-dom'
+import Login from "./containers/login"
+import Admin from "./containers/admin"
+import history from './history'
 
 export default class App extends Component {
 
-  handleClick = () => {
-    message.success('响应点击')
-  }
-
   render () {
     return (
-      <HashRouter>
+      <Router history={history}>
         <Switch> {/* /login/xxx   默认使用不完全匹配 | 使用第一个匹配的路由 */}
-          <Route path="/login" component={Login}/>
+          <Route path="/login" component={Login} exact/>
           <Route path="/" component={Admin}/>
         </Switch>
-      </HashRouter>
+      </Router>
     )
   }
 }
